48fdbc1e9498abf1c4e4b708345a6bc017764510
igorashs <igorashsirbu@gmail.com>
2020-04-30 15:02:20 +0300

    add indexController

---
+++ controllers/indexController.js
@@ -0,0 +1,15 @@
+const Message = require('../models/message');
+const User = require('../models/user');
+const debug = require('debug')('members-only:indexController');
+
+// GET all messages and render home page
+exports.getIndex = async (req, res, next) => {
+  try {
+    const messages = await Message.find().populate('user');
+
+    res.render('index', { title: 'Message Board', messages });
+  } catch (err) {
+    debug(err);
+    next();
+  }
+};
